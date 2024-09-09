bd100b1662219f03f8200e27f50e06c8e559efed
gavin <gavinmichori@gmail.com>
2024-09-09 16:24:13 +0300

    done

--- lib/testing/dog_test.py
+++ lib/testing/dog_test.py
@@ -14,6 +14,20 @@ class TestDog:
         fido = Dog()
         assert(type(fido) == Dog)
 
+    def test_sit_is_method(self):
+        '''sit() is an instance method'''
+        fido = Dog()
+        assert(type(fido.sit) == types.MethodType)
+
+    def test_sit_prints_the_dog_is_sitting(self):
+        '''sit() prints "The dog is sitting."'''
+        fido = Dog()
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        fido.sit()
+        sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "The dog is sitting.\n")
+
 class TestBark:
     '''Dog.bark() in dog.py'''
 
@@ -30,20 +44,3 @@ class TestBark:
         fido.bark()
         sys.stdout = sys.__stdout__
         assert(captured_out.getvalue() == "Woof!\n")
-
-class TestSit:
-    '''Dog.sit() in dog.py'''
-
-    def test_is_method(self):
-        '''is an instance method'''
-        fido = Dog()
-        assert(type(fido.sit) == types.MethodType)
-
-    def test_prints_the_dog_is_sitting(self):
-        '''prints "The dog is sitting."'''
-        fido = Dog()
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        fido.sit()
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "The dog is sitting.\n")
